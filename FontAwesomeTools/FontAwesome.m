adf7f60a17ceaa8851fabdbb6e3a565074202ee8
David Sweetman <davidsweetman@live.com>
2013-10-26 03:05:34 -0700

    Fix iOS 5 support

--- FontAwesomeTools/FontAwesome.m
+++ FontAwesomeTools/FontAwesome.m
@@ -55,49 +55,49 @@
 {
     NSAssert(fa_icon, @"You must specify an icon from font-awesome-codes.h.");
     
-#ifdef __IPHONE_6_0
-    if (!iconColor) { iconColor = [UIColor blackColor]; }
-    
-    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0.0);
-    NSAttributedString* attString = [[NSAttributedString alloc]
-                                     initWithString:fa_icon
-                                     attributes:@{NSFontAttributeName: [FontAwesome fontWithSize:iconSize],
-                                                  NSForegroundColorAttributeName : iconColor}];
-    // get the target bounding rect in order to center the icon within the UIImage:
-    NSStringDrawingContext *ctx = [[NSStringDrawingContext alloc] init];
-    CGRect boundingRect = [attString boundingRectWithSize:CGSizeMake(iconSize, iconSize) options:0 context:ctx];
-    // draw the icon string into the image:
-    [attString drawInRect:CGRectMake((imageSize.width/2.0f) - boundingRect.size.width/2.0f,
-                                     (imageSize.height/2.0f) - boundingRect.size.height/2.0f,
-                                     imageSize.width,
-                                     imageSize.height)];
-    UIImage *iconImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return iconImage;
-#else
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 6) {
+        if (!iconColor) { iconColor = [UIColor blackColor]; }
+        
+        UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0.0);
+        NSAttributedString* attString = [[NSAttributedString alloc]
+                                         initWithString:fa_icon
+                                         attributes:@{NSFontAttributeName: [FontAwesome fontWithSize:iconSize],
+                                                      NSForegroundColorAttributeName : iconColor}];
+        // get the target bounding rect in order to center the icon within the UIImage:
+        NSStringDrawingContext *ctx = [[NSStringDrawingContext alloc] init];
+        CGRect boundingRect = [attString boundingRectWithSize:CGSizeMake(iconSize, iconSize) options:0 context:ctx];
+        // draw the icon string into the image:
+        [attString drawInRect:CGRectMake((imageSize.width/2.0f) - boundingRect.size.width/2.0f,
+                                         (imageSize.height/2.0f) - boundingRect.size.height/2.0f,
+                                         imageSize.width,
+                                         imageSize.height)];
+        UIImage *iconImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return iconImage;
+    } else {
 #if DEBUG
-    NSLog(@" [ FontAwesomeTools ] Using lower-res iOS 5-compatible image rendering.");
+        NSLog(@" [ FontAwesomeTools ] Using lower-res iOS 5-compatible image rendering.");
 #endif
-    UILabel *iconLabel = [FontAwesome labelWithIcon:fa_icon size:iconSize color:iconColor];
-    UIImage *iconImage = nil;
-    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 1.0);
-    {
-        CGContextRef imageContext = UIGraphicsGetCurrentContext();
-        if (imageContext != NULL) {
-            UIGraphicsPushContext(imageContext);
-            {
-                CGContextTranslateCTM(imageContext,
-                                      (imageSize.width/2.0f) - iconLabel.frame.size.width/2.0f,
-                                      (imageSize.height/2.0f) - iconLabel.frame.size.height/2.0f);
-                [[iconLabel layer] renderInContext: imageContext];
+        UILabel *iconLabel = [FontAwesome labelWithIcon:fa_icon size:iconSize color:iconColor];
+        UIImage *iconImage = nil;
+        UIGraphicsBeginImageContextWithOptions(imageSize, NO, 1.0);
+        {
+            CGContextRef imageContext = UIGraphicsGetCurrentContext();
+            if (imageContext != NULL) {
+                UIGraphicsPushContext(imageContext);
+                {
+                    CGContextTranslateCTM(imageContext,
+                                          (imageSize.width/2.0f) - iconLabel.frame.size.width/2.0f,
+                                          (imageSize.height/2.0f) - iconLabel.frame.size.height/2.0f);
+                    [[iconLabel layer] renderInContext: imageContext];
+                }
+                UIGraphicsPopContext();
             }
-            UIGraphicsPopContext();
+            iconImage = UIGraphicsGetImageFromCurrentImageContext();
         }
-        iconImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return iconImage;
     }
-    UIGraphicsEndImageContext();
-    return iconImage;
-#endif
 }
 
 @end
